0292a57816f12b46a16e40e7bae33d96ea401a53
pebble-fish <pebble.fishie@gmail.com>
2021-07-29 13:35:52 -0400

    increased data group, added 'get rows'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,36 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {AgGridAngular} from 'ag-grid-angular';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild('agGrid') agGrid!: AgGridAngular;
 
-  columnDefs = [
-    { field: 'make', sortable: true, filter: true },
-    { field: 'model', sortable: true, filter: true },
-    { field: 'price', sortable: true, filter: true }
-];
+   columnDefs = [
+       {field: 'make', sortable: true, filter: true, checkboxSelection: true},
+       {field: 'model', sortable: true, filter: true},
+       {field: 'price', sortable: true, filter: true}
+   ];
 
-rowData: Observable<any[]> | undefined;
-  title: any;
+   rowData!: Observable<any[]>;
 
-constructor(private http: HttpClient) {
-}
+   constructor(private http: HttpClient) {
+   }
 
-ngOnInit(): void {
-    this.rowData = this.http.get<any[]>('https://www.ag-grid.com/example-assets/small-row-data.json');
-}
+   ngOnInit(): void {
+    this.rowData = this.http.get<any[]>('https://www.ag-grid.com/example-assets/row-data.json');
+   }
+
+  getSelectedRows(): void {
+      const selectedNodes = this.agGrid.api.getSelectedNodes();
+      const selectedData = selectedNodes.map(node => node.data);
+      const selectedDataStringPresentation = selectedData.map(node => `${node.make} ${node.model}`).join(', ');
+
+      alert(`Selected nodes: ${selectedDataStringPresentation}`);
+  }
 }
